3a131c91eda201d2df5a1452e490ccace9c7b465
GayathriKalanadhabatla <gayathri.kalanadhabatla@wavemaker.com>
2024-01-05 15:07:28 +0530

    modified config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 10000,
-  pageLoadTimeout: 40000,
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    defaultCommandTimeout: 10000,
+   pageLoadTimeout: 40000,
+  reporter:'mochawesome',
+  reporterOptions:{
+    reportFilename: 'TurboMobilesReport',
+    quiet: true,
+    "overwrite": true,
+    "html": true,
+    "json": true}
+    
   },
 });
+
+
+
